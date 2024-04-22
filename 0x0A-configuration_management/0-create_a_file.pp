915f13b8adcf29d0796a1dceeb176704914bee06
Youssef Oussaid <oussaid-youssef@outlook.com>
2024-04-22 22:10:31 +0100

    a

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-
+#A manifest to create a file
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I like puppet",
+}
